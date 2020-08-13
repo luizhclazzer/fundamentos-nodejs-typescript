be99f6f2d830700269d93486eb7da6030b47dbeb
Luiz Henrique Clazzer <luiz.clazzer@NW071.vms.com.br>
2020-08-13 00:52:35 -0300

    Cadastro e listagem de transações, calculando o balanço

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateRepositoryDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -9,20 +15,38 @@ interface Balance {
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance = {
+    income: 0,
+    outcome: 0,
+    total: 0,
+  };
+
   constructor() {
     this.transactions = [];
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateRepositoryDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    if (type === 'income') {
+      this.balance.income += value;
+    }
+    if (type === 'outcome') {
+      this.balance.outcome += value;
+    }
+    this.balance.total = this.balance.income - this.balance.outcome;
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
